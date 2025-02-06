abdf91eff3cee1b8a017aa071ebcd906d2bf9584
Lucas Silva <kucasss19@gmail.com>
2025-02-06 16:11:44 -0300

    add funcionalidades display

--- includes/utils/gerenciar_display.h
+++ includes/utils/gerenciar_display.h
@@ -5,6 +5,6 @@
 #include "ssd1306.h"
 
 void enviar_letra_para_display(char c, ssd1306_t *ssd);
-void enviar_mensagem_para_display(char *mensagem, ssd1306_t *ssd);
+void enviar_mensagem_para_display(char *mensagem1, char *mensagem2, ssd1306_t *ssd);
 
 #endif
--- src/utils/gerenciar_display.c
+++ src/utils/gerenciar_display.c
@@ -3,24 +3,31 @@
 #include "matriz_leds.h"
 #include "gerenciar_display.h"
 
-const uint r = 10, b = 10, g = 10; // valores de r, g e b para a matriz de leds
+const uint r = 5, b = 5, g = 5; // valores de r, g e b para a matriz de leds
 
+/*
+* Função para enviar um caracter para o display
+*/
 void enviar_letra_para_display(char c, ssd1306_t *ssd) {
 
     if (c >= '0' && c <= '9') {
         desenhar_numero_na_matriz_de_leds(r, g, b, c);
     }
 
+    ssd1306_fill(ssd, false); // Limpa o display
     ssd1306_draw_char(ssd, c, 60, 30);
     ssd1306_send_data(ssd); // Atualiza o display
 
-
 }
 
-void enviar_mensagem_para_display(char *mensagem, ssd1306_t *ssd) {
+/*
+* Função para enviar uma string para o display
+*/
+void enviar_mensagem_para_display(char *mensagem1, char *mensagem2, ssd1306_t *ssd) {
 
-
-    ssd1306_draw_string(ssd, mensagem, 20, 30);
+    ssd1306_fill(ssd, false); // Limpa o display
+    ssd1306_draw_string(ssd, mensagem1, 20, 30); 
+    ssd1306_draw_string(ssd, mensagem2, 20, 40);
     ssd1306_send_data(ssd); // Atualiza o display
 
 

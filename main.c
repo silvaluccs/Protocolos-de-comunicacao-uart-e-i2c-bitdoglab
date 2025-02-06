6e52178c44cf8a8e87614c51d714811fbef27389
Lucas Silva <kucasss19@gmail.com>
2025-02-06 11:39:00 -0300

    add função debouce

--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 #include "ws2812.pio.h" // biblioteca para usar ws2812
 #include "matriz_leds.h"
 #include "setup.h"
+#include "debouce.h"
 
 const uint pino_led_azul = 12; // pino do led azul
 const uint pino_led_verde = 11;
 const uint pino_botao_a = 5; // pino do botao A
 const uint pino_botao_b = 6; // pino do botao B
 const uint pino_matriz_leds = 7; // pino da matriz de leds
+
+volatile uint32_t ultimo_tempo = 0;
 
 
 void gpio_irq_handler(uint gpio, uint32_t events); // prototipo da função para tratar a interrupção dos botoes
